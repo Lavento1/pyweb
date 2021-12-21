cf05a1b7da9b748c020906d6a3538845bf5bcd50
Lavento1 <wlsxo4603@naver.com>
2021-12-21 12:48:33 +0900

    Base Template + bootstrap 추가

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,11 @@ app_name = 'board'
 
 urlpatterns = [
     # 127.0.0.1:8000/index
-    path('', views.index),
+    # 질문 목록
+    path('', views.index, name='index'),
+    # 질문 / 답변 상세
     path('<int:question_id>/', views.detail, name='detail'),
+    # 질문 등록
+    path('question/create/', views.question_create, name='question_create'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
