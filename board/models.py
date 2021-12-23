a5fcf41eb54b353842725a8210d0b7fef0bff907
Lavento1 <wlsxo4603@naver.com>
2021-12-23 12:54:20 +0900

    pyweb 21/12/23

--- board/models.py
+++ board/models.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 # Create your models here.
 class Question(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)  # 제목 칼럼
     content = models.TextField()                # 내용
     create_date = models.DateTimeField()        # 작성일
@@ -12,6 +14,7 @@ class Question(models.Model):
 
 
 class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)    # 외래키, 제목
     content = models.TextField()    # 답변 내용
     create_date = models.DateTimeField()    # 답변 작성일
--- board/views.py
+++ board/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
@@ -24,12 +25,14 @@ def detail(request, question_id):
     return render(request, 'board/detail.html', {'question': question})
 
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == "POST":
         form = QuestionForm(request.POST)   # 자료 전달받음(request.POST)
         if form.is_valid():
             question = form.save(commit=False)  # 가저장(날짜를 전달받지 못했기에 가저장)
             question.create_date = timezone.now()   # 날짜 시간 저장
+            question.author = request.user  # 세션에 있는 유저 저장
             question.save()     # 실제 저장
             return redirect('board:index')  # 이동할 경로(앱네임 사용) 저장
     else:
@@ -37,6 +40,7 @@ def question_create(request):
     return render(request, 'board/question_form.html', {'form': form})
 
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     # 답변 등록
     question = Question.objects.get(id=question_id) # 해당 id의 질문 객체 생성
@@ -45,6 +49,7 @@ def answer_create(request, question_id):
         if form.is_valid():
             answer = form.save(commit=False)  # 내용만 저장됨
             answer.create_date = timezone.now() # 작성일
+            answer.author = request.user    # 세션 발급
             answer.question = question  # 외래키 질문 저장
             answer.save()
             return redirect('board:detail', question_id=question.id)
@@ -53,3 +58,28 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
 
     return render(request, 'board/detail.html', context)
+
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    # 질문 수정
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)  # 수정된 질문 가저장
+            question.author = request.user  # 세션 발급
+            question.modify_date = timezone.now()   # 수정일
+            question.save()
+            return redirect('board:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)  # instance를 쓰면 폼에 내용이 채워짐
+    return render(request, 'board/question_form.html', {'form': form})
+
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    # 질문 삭제
+    question = get_object_or_404(Question, pk=question_id)
+    question.delete()   # 질문 삭제
+    return redirect('board:index')
